1a5b3aa373a65e4b2768ae90ccfa9e71b9fde608
S0l0m4n <sam@stickerbrush>
2022-09-21 22:54:03 +0300

    set1: C3: Calculate number of alpha chars in each decrypted text

--- set1/c3_single_byte_xor_cipher.py
+++ set1/c3_single_byte_xor_cipher.py
@@ -9,25 +9,47 @@
 #   "Cooking MC's like a pound of bacon"
 # achieved with a key of 0x58.
 
+MIN_ALPHA = 65  # ord('A') = 65
+MAX_ALPHA = 122 # ord('z') = 122
+
 text_enc = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 
 def xor(x, y):
     return hex(int(x, 16) ^ int(y, 16))
 
-def singleByteCipher(text, key):
+# Usage: singleByteCipherHex("1b3737", '0x50') = '4b6767'
+def singleByteCipherHex(text, key):
     # create the cipher by duplicating the key character to be as long as text
     key_byte_count = len(text) // 2 + len(text) % 2
     cipher = key_byte_count * key[2:] # drop the '0x' part of the key string
     # xor each byte to decrypt text: this will be in hex
     text_dec = xor(text, cipher)
-    text_dec = text_dec[2:] # drop the '0x' at the start
-    # try to convert the output to characters
+    return text_dec[2:] # drop the '0x' at the start
+
+# Usage: splitHexIntoArray("4b6767") = [75, 103, 103]
+def splitHexIntoIntArray(x):
+    return [ int(x[i:i+2],16) for i in range(0, len(x), 2) ]
+
+# Usage: [*mapIsAlphaOverIntArray([75, 123, 103])] = [True, False, True]
+def mapIsAlphaOverIntArray(hex_array):
+    return map(lambda x: (MIN_ALPHA <=x) and (x <= MAX_ALPHA), hex_array)
+
+def printHexAsByteString(hex_output):
     try:
-        output = bytes.fromhex(text_dec)
-    except ValueError:
-        output = b'0'
-    return output
+        byte_string = bytes.fromhex(hex_output)
+    except ValueError: 
+        byte_string = b'0'
+    return byte_string
 
 for c in range(0x00, 0xff):
     print("{0:3d}. [0x{1:02x}]".format(c, c), end=' ')
-    print(singleByteCipher(text_enc, hex(c)))
+    # encrypt the text with single-byte XOR cipher
+    hex_output = singleByteCipherHex(text_enc, hex(c))
+    hex_array = splitHexIntoIntArray(hex_output)
+    # count how many alphabetical characters in the text
+    # NB: we use `list` because otherwise the map object will be consumed
+    alpha_list = list(mapIsAlphaOverIntArray(hex_array))
+    # reveal sum of alphabetical characters in the text
+    # NB: `sum` can add boolean values
+    print("#alpha = {} {}".format(
+        sum(alpha_list), printHexAsByteString(hex_output)))
